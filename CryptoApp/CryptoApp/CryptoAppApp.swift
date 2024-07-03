964a2e0ddaf5283fcf3e7cfe2b46777dfa94b3b9
JLuisRod96 <88519400+Jose96Luis@users.noreply.github.com>
2024-07-03 12:47:16 -0600

    InitialCommit

---
+++ CryptoApp/CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by José Rodriguez Romero on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
